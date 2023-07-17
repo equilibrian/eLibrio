b2ba41deb036e6422872516be386ad6b4d6196b1
th2empty <th2empty@gmail.com>
2023-07-18 02:17:59 +0500

    init

---
+++ app/src/main/java/ru/equilibrian/elibrio/HubScreen.kt
@@ -0,0 +1,25 @@
+package ru.equilibrian.elibrio
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.equilibrian.elibrio.ui.theme.ELibrioTheme
+
+@Composable
+fun HubScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Hub screen in development...", modifier = Modifier.align(Alignment.Center))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HubScreenPreview() {
+    ELibrioTheme {
+        HubScreen()
+    }
+}
---
+++ app/src/main/java/ru/equilibrian/elibrio/LibraryScreen.kt
@@ -0,0 +1,28 @@
+package ru.equilibrian.elibrio
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.equilibrian.elibrio.ui.theme.ELibrioTheme
+
+/*class LibraryScreen {
+}*/
+
+@Composable
+fun LibraryScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Library screen in development", modifier = Modifier.align(Alignment.Center))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LibraryScreenPreview() {
+    ELibrioTheme {
+        LibraryScreen()
+    }
+}
